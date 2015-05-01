beaa7da384bc370b91f101b1946db441b00c7daa
tmoreno <tmorenobernal@yahoo.es>
2015-05-01 18:58:13 +0200

    Implemented code for pass all tests

--- src/main/java/com/tmoreno/stringcalculator/StringCalculator.java
+++ src/main/java/com/tmoreno/stringcalculator/StringCalculator.java
@@ -2,8 +2,13 @@ package com.tmoreno.stringcalculator;
 
 public class StringCalculator {
 
-	public int add(String string) {
-		return 0;
+	public int add(String numbers) {
+		if (numbers.isEmpty()) {
+			return 0;
+		}
+		else {
+			return Integer.parseInt(numbers);
+		}
 	}
 
 }
