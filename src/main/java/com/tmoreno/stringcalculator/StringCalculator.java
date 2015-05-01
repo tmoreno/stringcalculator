0262250623cdef292c3c5719fecd8a6551ecbdec
tmoreno <tmorenobernal@yahoo.es>
2015-05-01 22:44:21 +0200

    Implemented code for pass all tests

--- src/main/java/com/tmoreno/stringcalculator/StringCalculator.java
+++ src/main/java/com/tmoreno/stringcalculator/StringCalculator.java
@@ -1,5 +1,9 @@
 package com.tmoreno.stringcalculator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class StringCalculator {
 
 	public int add(String input) {
@@ -7,7 +11,12 @@ public class StringCalculator {
 			return 0;
 		}
 		
-		String numbers[] = input.split(",");
+		List<String> numbers = new ArrayList<>(); 
+
+		String inputWhitoutCommas[] = input.split(",");
+		for (String item : inputWhitoutCommas) {
+			numbers.addAll(Arrays.asList(item.split("\n")));
+		}
 		
 		int result = 0;
 		for (String number : numbers) {
