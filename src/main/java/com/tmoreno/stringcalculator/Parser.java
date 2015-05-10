7ad7ddcae13617f71320b83caf89c8b905c6b4a0
tmoreno <tmorenobernal@yahoo.es>
2015-05-10 16:40:34 +0200

    Renames

--- src/main/java/com/tmoreno/stringcalculator/Parser.java
+++ src/main/java/com/tmoreno/stringcalculator/Parser.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Parser {
 
+	private static final String BEGIN_LINE_DELIMITER = "//";
 	private static final String DEFAULT_DEMILITER = ",";
 
 	private String delimiter;
@@ -19,9 +20,9 @@ public class Parser {
 		delimiter = DEFAULT_DEMILITER;
 		input = newInput;
 		
-		if (newInput.startsWith("//")) {
-			delimiter = newInput.charAt(2) + "";
-			input = newInput.substring(4);
+		if (newInput.startsWith(BEGIN_LINE_DELIMITER)) {
+			delimiter = newInput.charAt(BEGIN_LINE_DELIMITER.length()) + "";
+			input = newInput.substring(BEGIN_LINE_DELIMITER.length() + delimiter.length() + 1);
 		}
 		
 		if (input.indexOf("\n" + delimiter) > 0) {
